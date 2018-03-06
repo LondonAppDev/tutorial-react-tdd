54666a1353684dffa9c21663663bc480ae2cd215
Mark Winterbottom <mark@londonappdeveloper.com>
2018-03-06 13:17:01 +0000

    Added working list of products with unit tests.

---
+++ src/components/ProductList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+
+function ProductList(props) {
+  return (
+    <ul>
+      {
+        props.products.map(product => (
+          <li key={product.id}>
+            {product.name}
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+ProductList.propTypes = {
+  products: PropTypes.array.isRequired
+};
+
+export default ProductList;
